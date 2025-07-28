e648088e5942aac2242686c0895bc8440c76638e
AYUSHRAJ5354n <167030308+AYUSHRAJ5354n@users.noreply.github.com>
2025-07-28 19:58:25 +0530

    Update processor.py

--- scripts/processor.py
+++ scripts/processor.py
@@ -10,22 +10,19 @@ class VideoProcessor:
     def convert_to_doc(self, video_path):
         """Convert video to DOC with thumbnail"""
         try:
-            # Generate thumbnail
             thumb_path = os.path.join(self.temp_dir, f"thumb_{os.path.basename(video_path)}.jpg")
             subprocess.run([
                 'ffmpeg', '-i', video_path,
                 '-ss', '00:00:05', '-vframes', '1', thumb_path
             ], check=True)
             
-            # Create DOC
             doc_path = os.path.splitext(video_path)[0] + '.docx'
             doc = Document()
             doc.add_heading('Video Content', level=1)
-            doc.add_picture(thumb_path, width=docx.shared.Inches(4.5))
+            doc.add_picture(thumb_path, width=6)  # 6 inches width
             doc.add_paragraph("Video will be sent separately in Telegram")
             doc.save(doc_path)
             
-            # Cleanup
             os.remove(thumb_path)
             return doc_path
             
